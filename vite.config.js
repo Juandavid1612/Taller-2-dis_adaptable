5f4bf5b34b4ce94075693cee10b7f8662db0ab4d
Juan David Ríos Sánchez <70483883+Juandavid1612@users.noreply.github.com>
2026-09-12 01:58:24 -0500

    cambio de base

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-
-  base: '/retro-gaming-fest/',
+  plugins: [react(), tailwindcss()],
+  base: '/Taller-2-dis_adaptable/',
 })
